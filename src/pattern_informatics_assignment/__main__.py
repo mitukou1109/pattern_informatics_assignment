62c5707a57ec2bc0b360d0bdd0e8cfeab3ba9f43
mitukou1109 <mitukou1109@gmail.com>
2023-12-14 09:09:12 +0900

    Let user select target

--- src/pattern_informatics_assignment/__main__.py
+++ src/pattern_informatics_assignment/__main__.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 from sklearn.datasets import load_digits, make_circles, make_classification, make_moons
 from sklearn.model_selection import train_test_split
@@ -15,10 +17,20 @@ PERCEPTRON_LINEAR = 0
 PERCEPTRON_QUADRATIC = 1
 PERCEPTRON_CUBIC = 2
 MULTINOMINAL_LOGISTIC = 3
-KNN_2D = 4
-KNN_MULTICLASS = 5
+KNN_CLASSIFICATION = 4
+KNN_CIRCLES = 5
+KNN_MOONS = 6
+KNN_MULTICLASS = 7
 
-target = KNN_MULTICLASS
+if len(sys.argv) > 1:
+    target = int(sys.argv[1])
+else:
+    target = int(
+        input(
+            "0: Perceptron - Linear, 1: Perceptron - Quadratic, 2: Perceptron - Cubic, 3: Multinominal Logistic, \
+             \n4: KNN - make_classification, 5: KNN - make_circles, 6: KNN - make_moons 7: KNN - Multiclass >> "
+        )
+    )
 
 if target <= PERCEPTRON_CUBIC:
     if target == PERCEPTRON_LINEAR:
@@ -56,17 +68,20 @@ elif target == MULTINOMINAL_LOGISTIC:
     classifier.train(X_train, y_train, 1000)
     classifier.test(X_test, y_test)
 
-elif target == KNN_2D:
-    # X, y = make_classification(
-    #     n_samples=200,
-    #     n_features=2,
-    #     n_informative=2,
-    #     n_redundant=0,
-    #     n_clusters_per_class=1,
-    #     random_state=15,
-    # )
-    # X, y = make_circles(n_samples=200, noise=0.1, random_state=42)
-    X, y = make_moons(n_samples=200, noise=0.3)
+elif target <= KNN_MOONS:
+    if target == KNN_CLASSIFICATION:
+        X, y = make_classification(
+            n_samples=200,
+            n_features=2,
+            n_informative=2,
+            n_redundant=0,
+            n_clusters_per_class=1,
+            random_state=15,
+        )
+    elif target == KNN_CIRCLES:
+        X, y = make_circles(n_samples=200, noise=0.1, random_state=42)
+    elif target == KNN_MOONS:
+        X, y = make_moons(n_samples=200, noise=0.3)
     classifier = KNNClassifier(k=16, n_classes=2)
     classifier.train(X, y)
     util.plot_decision_boundary(classifier, X, y)
@@ -86,3 +101,6 @@ elif target == KNN_MULTICLASS:
         classifier.train(X_train, y_train)
         classifier.test(X_test, y_test)
     util.plot_line(ks, accuracy, xlabel="k", ylabel="accuracy")
+
+else:
+    raise ValueError("Invalid target")
